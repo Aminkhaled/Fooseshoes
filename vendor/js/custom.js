83b37a1582bd9fe8eb7e9ec120f43996ebfa17a7
Amen Khaled <Aminkhaled@users.noreply.github.com>
2020-09-15 15:21:45 +0200

    we added offers sections

--- vendor/js/custom.js
+++ vendor/js/custom.js
@@ -1,40 +1,4 @@
-    //
-    //
-    // $(document).ready(function(){
-    //     $(".navbar-nav .nav-link").on("click", function(){
-    //         $(".navbar-nav").find(".active").removeClass("active");
-    //         $(this).addClass("active");
-    //     });
-    //     $(".owl-carousel").owlCarousel({
-    //         animateOut: 'slideOutDown',
-    //         animateIn: 'flipInX',
-    //         items:1,
-    //         smartSpeed:100,
-    //         autoplay:true
-    //     });
-    //     $('.portfolio-menu ul li').click(function(){
-    //         $('.portfolio-menu ul li').removeClass('active');
-    //         $(this).addClass('active');
-    //
-    //         var selector = $(this).attr('data-filter');
-    //         $('.portfolio-item').isotope({
-    //             filter:selector
-    //         });
-    //         return  false;
-    //     });
-    //
-    //         var popup_btn = $('.popup-btn');
-    //         popup_btn.magnificPopup({
-    //             type : 'image',
-    //             gallery : {
-    //                 enabled : true
-    //             }
-    //         });
-    //
-    //
-    //
-    //
-    //
-    //
-    //
-    // });
+
+    $(document).ready(function(){
+
+    });
